ec882d482751368aa6aa10f4f1195a32ef89e213
Mohit Mehral <mohitmehral@gmail.com>
2023-09-24 02:01:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ app = create_app()
 
 if __name__ == '__main__':
     host = os.getenv('FLASK_HOST', '127.0.0.1')
-    port = os.getenv('FLASK_PORT', '5000')
+    port = os.getenv('FLASK_PORT', '5001')
     app.run(host=host, port=int(port))
